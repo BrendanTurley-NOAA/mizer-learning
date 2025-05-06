3d0ba784d3a8891310739c57e7c81e2d44caff03
Brendan Turley <brendan.turley@noaa.gov>
2025-05-06 17:20:13 -0400

    finish section

--- code/part1.R
+++ code/part1.R
@@ -5,19 +5,28 @@
 ### Part 1: Understand
 ### Observed size spectrum
 
-install.packages(c("mizer", "tidyverse", "plotly", "remotes", "usethis",
-                   "rmarkdown", "rstudioapi"))
+
+### Introduction
+
+# install.packages(c("mizer", "tidyverse", "plotly", "remotes", "usethis",
+#                    "rmarkdown", "rstudioapi"))
 library(tidyverse)
 
-download.file("https://github.com/gustavdelius/mizerCourseNew/raw/master/understand/size-data.rds",
-              destfile = "data/size-data.rds")
+
+## The data
+
+# download.file("https://github.com/gustavdelius/mizerCourseNew/raw/master/understand/size-data.rds",
+              # destfile = "data/size-data.rds")
 
 size_data <- readRDS("data/size-data.rds")
 
 str(size_data)
 unique(size_data$species)
 
-hist(size_data$weight, xlab = 'weight [g]', ylab = 'number of fish', las = 1)
+
+## Histogram
+
+hist(size_data$weight, xlab = 'weight [g]', ylab = 'number of fish', las = 1, nclass = 30)
 plot(hist(size_data$weight)$mids, hist(size_data$weight)$counts, log="y", type='h', lwd=10, lend=2)
 
 p <- ggplot(size_data) +
@@ -63,12 +72,16 @@ p3
 p2 + scale_x_log10()
 
 
+## Density in size
+## Binning
 
 data_with_bins <- size_data |>
   mutate(bin = cut(weight, breaks = bin_breaks, right = FALSE,
                    labels = FALSE))
 head(data_with_bins)
 
+
+## Number density
 
 binned_numbers <- data_with_bins |> 
   group_by(bin) |> 
@@ -132,4 +145,85 @@ p_number_density
 ## end exercise #2
 
 
-### Fitting a power law
+## Fitting a power law
+
+model <- lm(log(Number_density) ~ log(bin_midpoint), data = binned_numbers)
+model
+summary(model)
+
+
+ggplot(binned_numbers, aes(x = bin_midpoint, y = Number_density)) +
+  geom_line() + 
+  scale_x_log10(name = "Weight [g]") + 
+  scale_y_log10(name = "Number density [1/g]") +
+  geom_smooth(method = 'lm')
+
+
+n <- nrow(size_data)
+w_min <- min(size_data$weight)
+lambda <- 1 + n / sum(log(size_data$weight / w_min))
+lambda
+
+
+
+## Biomass density
+
+binned_biomass <- data_with_bins |> 
+  group_by(bin) |> 
+  summarise(Biomass = sum(weight)) |>
+  mutate(bin_start = bin_breaks[-length(bin_breaks)],
+         bin_end = bin_breaks[-1],
+         bin_width = bin_end - bin_start,
+         bin_midpoint = exp((log(bin_start) + log(bin_end)) / 2),
+         Biomass_density = Biomass / bin_width)
+
+ggplot(binned_biomass, aes(x = bin_midpoint, y = Biomass_density)) +
+  geom_line() + 
+  scale_x_log10(name = "Weight [g]") + 
+  scale_y_log10(name = "Biomass density") +
+  geom_smooth(method = 'lm')
+
+
+lm(log(Biomass_density) ~ log(bin_midpoint), data = binned_biomass)
+
+
+binned_data <- 
+  left_join(binned_numbers, binned_biomass,
+            by = c("bin", "bin_start", "bin_end", "bin_width", 
+                   "bin_midpoint")) |>
+  mutate(bin_width_log_w = log10(bin_end) - log10(bin_start),
+         Number_density_log_w = Number / bin_width_log_w,
+         Biomass_density_log_w = Biomass / bin_width_log_w)
+
+
+long_data <- binned_data |>
+  pivot_longer(cols = contains("density"),
+               names_to = "Type", values_to = "Density")
+
+ggplot(long_data, aes(x = bin_midpoint, y = Density, colour = Type)) +
+  geom_line() + 
+  scale_x_log10(name = "Weight [g]") + 
+  scale_y_log10(name = "Density") +
+  geom_smooth(method = 'lm', se = F)
+
+
+### Excercise #3
+names(binned_data)
+
+lm(log(Biomass_density_log_w) ~ log(bin_midpoint), data = binned_data)
+### end exercise #3
+
+
+## Sheldon's observation
+# Sheldon's density = biomass density in long weight
+## Size spectra of individual species
+
+p <- ggplot(size_data) +
+  geom_density(aes(weight, stat(count), colour = species), adjust = 4) +
+  geom_density(aes(weight, stat(count)), colour = "black", lwd = 1.2, adjust = 4) +
+  scale_x_continuous(trans = "log10", name = "Weight [g]") +
+  scale_y_continuous(trans = "log10", limits = c(1, NA), name = "Number density in log w")
+
+plotly::ggplotly(p)
+
+
