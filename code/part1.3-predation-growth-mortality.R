846f0d4b8dd36734ffc8f5a2da60c3acdb27dd9b
Brendan Turley <brendan.turley@noaa.gov>
2025-05-23 17:37:23 -0400

    next part

---
+++ code/part1.3-predation-growth-mortality.R
@@ -0,0 +1,7 @@
+### MIZER online course
+### https://mizer.course.sizespectrum.org/
+### May 2025
+
+### Part 1: Understand
+### Predation, growth and mortality
+## https://mizer.course.sizespectrum.org/understand/predation-growth-and-mortality.html
